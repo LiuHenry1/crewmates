8c40d0ff7ff73c8fecb335439c227c283aa1dd03
Henry Liu <hliu4176@gmail.com>
2023-10-31 18:32:11 -0500

    Feature: Add structure and content

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,15 @@
 const Home = () => {
-  return <>This is the Home page</>;
+  return (
+    <>
+      <h1>Welcome to the Crewmate Creator!</h1>
+      <p>
+        Here is where you can create your own set of crewmates before sending
+        them off into space!
+      </p>
+      <img src="src/assets/crewmates.png" width={500} height={400}/>
+      <img src="src/assets/spaceship.png" width={500} height={400}/>
+    </>
+  );
 };
 
 export default Home;
