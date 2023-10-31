5dda846ba64319ae85cafc2959010a3ab05ced11
Henry Liu <hliu4176@gmail.com>
2023-10-31 18:21:53 -0500

    Feature: Create a navigation bar and an outlet
    Create a navigation bar with links to the other pages. Add an outlet to
    display the corresponding page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
-import './App.css'
+import { useRoutes, Link } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import Form from "./components/Form";
+import Gallery from "./components/Gallery";
 
 function App() {
-
+  let main = useRoutes([
+    { 
+      path: "/", 
+      element: <Home /> },
+    {
+      path: "/new",
+      element: <Form />,
+    },
+    {
+      path: "/gallery",
+      element: <Gallery />,
+    },
+  ]);
 
   return (
-    <>
-     
-    </>
-  )
+    <div className="App">
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/new">Create a new Crewmate!</Link>
+        <Link to="/gallery">Crewmate Gallery</Link>
+      </nav>
+      <div className="main">
+        {main}
+      </div>
+
+    </div>
+  );
 }
 
-export default App
+export default App;
