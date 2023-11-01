149df2f969ff89d2188e6eb171c714f7d7a9c0f5
Henry Liu <hliu4176@gmail.com>
2023-10-31 19:47:34 -0500

    Build: Install supabase

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,5 +1,10 @@
 const Gallery = () => {
-  return <>This is the Gallery page</>
+  const crewMates = null;
+
+  return (<>
+    <h1>Your Crewmates gallery</h1>
+    {crewMates ?  crewMates : <div>You haven't made a crewmate yet!</div>}
+  </>)
 }
 
 export default Gallery;
