6b004f2f5cb4bb5fb7dd9eaebd67b1ee547dd57f
Henry Liu <hliu4176@gmail.com>
2023-10-31 19:41:10 -0500

    Feature: Add structure and content
    Implement interactivity with database later

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,31 @@
 const Form = () => {
-  return <>This is the Form page</>
-}
+  const colors = [
+    "Red",
+    "Green",
+    "Blue",
+    "Purple",
+    "Yellow",
+    "Orange",
+    "Pink",
+    "Rainbow",
+  ];
+  const colorInputs = colors.map((color) => (
+    <>
+      <input type="radio" name={color} value={color} />
+      <label for={color}>{color}</label>
+    </>
+  ));
+  return (
+    <>
+      <label for="name">Name</label>
+      <input type="text" id="name" name="name" />
+      <label for="speed">Speed (mph)</label>
+      <input type="text" id="speed" name="speed" />
+      <div>
+        {colorInputs}
+      </div>
+    </>
+  );
+};
 
 export default Form;
