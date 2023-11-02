833cb194433fca44373c55e3222e7a21c1a161e8
Henry Liu <hliu4176@gmail.com>
2023-11-02 09:31:49 -0500

    Feature: Add link to edit page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,17 @@
-const Card = ({crewmate}) => {
+import { Link } from "react-router-dom";
+
+const Card = ({ crewmate }) => {
   return (
     <div className="crewmate">
       <img src="src/assets/crewmate.png" width={200} height={200} />
       <h4>Name: {crewmate.name} </h4>
       <h4>Speed: {crewmate.speed} </h4>
       <h4>Color: {crewmate.color} </h4>
+      <Link to={`/edit/${crewmate.id}`}>
+        <button type="button">Edit Crewmate</button>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
 export default Card;
