c5a451868827700ced7cc02b034e1242ec85c7ac
Henry Liu <hliu4176@gmail.com>
2023-11-03 10:53:33 -0500

    Feature: Clicking on a card will display detailed view

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,12 +3,14 @@ import { Link } from "react-router-dom";
 const Card = ({ crewmate }) => {
   return (
     <div className={`crewmate ${crewmate.color.toLowerCase()}`}>
-      <img src="src/assets/crewmate.png" width={200} height={200} />
-      <h4>Name: {crewmate.name} </h4>
-      <h4>Speed: {crewmate.speed} </h4>
-      <h4>Color: {crewmate.color} </h4>
+      <Link to={`/detail/${crewmate.id}`}>
+        <img src="src/assets/crewmate.png" width={200} height={200} />
+        <h4>Name: {crewmate.name} </h4>
+        <h4>Speed: {crewmate.speed} </h4>
+        <h4>Color: {crewmate.color} </h4>
+      </Link>
       <Link to={`/edit/${crewmate.id}`}>
-        <input type="submit" className="button-84" value="Edit/Delete"/>
+        <input type="submit" className="button-84" value="Edit/Delete" />
       </Link>
     </div>
   );
